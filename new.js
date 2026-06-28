71e958ebd3a3287d7ca94f7a36272d2aaacaad1b
Milena de Bastos <milenabmiranda1234@gmail.com>
2026-06-28 18:04:56 +0000

    correction

--- new.js
+++ new.js
@@ -3,5 +3,5 @@ const Btn = document.getElementById("btn");
 
 Btn.addEventListener("click", () => {
     Coin.style.display = "block";
-     Btn.src = "https://mario.wiki.gallery/images/thumb/c/cc/PN_Block.png/120px-PN_Block.png"; // nova imagem do botão
+     Btn.src = "https://mario.wiki.gallery/images/thumb/c/cc/PN_Block.png/120px-PN_Block.png";
 });
